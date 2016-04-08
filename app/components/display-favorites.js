db36f460c6e2fc0f80000352c8d66cb3e04b16c5
sara <sara.wagner23q@gmail.com>
2016-04-08 14:43:22 -0700

    add remove favorite but removes entire series

--- app/components/display-favorites.js
+++ app/components/display-favorites.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
     favorites: Ember.inject.service(),
+
     actions: {
       remove(favorite) {
         this.get('favorites').remove(favorite);
