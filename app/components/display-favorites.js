c1439628235c2e1a33ef4558f3c09d2f06b88c83
sara <sara.wagner23q@gmail.com>
2016-04-08 14:32:15 -0700

    add styling

--- app/components/display-favorites.js
+++ app/components/display-favorites.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Component.extend({
     favorites: Ember.inject.service(),
     actions: {
-      removeFromFavorites(favorite) {
+      remove(favorite) {
         this.get('favorites').remove(favorite);
       }
     }
